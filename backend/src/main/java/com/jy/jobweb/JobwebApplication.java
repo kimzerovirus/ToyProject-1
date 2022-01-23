39188b1beb6f5dbd27a43f712b22323d4d67ad8e
jaeyeun95 <jaeyeun95@gmail.com>
2022-01-23 16:14:15 +0900

    spring 세팅 추가 swagger 세팅추가

---
+++ backend/src/main/java/com/jy/jobweb/JobwebApplication.java
@@ -0,0 +1,13 @@
+package com.jy.jobweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobwebApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/jy/jobweb/JobwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.jobweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

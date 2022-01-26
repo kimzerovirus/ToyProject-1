98ceab25fc443f9a6e64a11a6736baf3213d41ec
jaeyeun <jaeyeun95@gmail.com>
2022-01-26 16:48:54 +0900

    사용자 모델, 엔티티 추가

--- backend/src/main/java/com/jy/jobweb/user/dto/UserDTO.java
+++ backend/src/main/java/com/jy/jobweb/user/dto/UserDTO.java
@@ -1,4 +1,34 @@
 package com.jy.jobweb.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+
+    private String id;
+    private String pwd;
+    private String name;
+    // user_lv -> 기업관계자(c), 헤드헌터(h)
+    private String grade;
+
+//    private String nickname;
+//    private String email;
+//    private String email_cert;
+//
+//
+//    private String birthday;
+//    private String gender;
+//    private String phone;
+//    private String regist_date;
+//    private String update_date;
+//    private String del_yn;
+//    private String bank_acct;
+
+
 }
--- backend/src/main/java/com/jy/jobweb/user/model/UserEntity.java
+++ backend/src/main/java/com/jy/jobweb/user/model/UserEntity.java
@@ -1,4 +1,45 @@
 package com.jy.jobweb.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+//@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = "nickname")})
 public class UserEntity {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String pwd;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String grade; //user_lv -> 기업관계자(c), 헤드헌터(h)
+
+//    private String nickname;
+//    private String email;
+//    private String email_cert;  // 이메일 인증 여부
+
+//
+//    private String birthday;
+//    private String gender;
+//    private String phone;
+//    private String regist_date;
+//    private String update_date;
+//    private String del_yn;
+//    private String bank_acct;
+
+
 }

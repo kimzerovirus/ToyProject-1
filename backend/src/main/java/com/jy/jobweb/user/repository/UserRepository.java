9952474d980d273cdd0e43c9b3e167730de6cc5a
jaeyeun <jaeyeun95@gmail.com>
2022-01-28 22:06:57 +0900

    security 수정

--- backend/src/main/java/com/jy/jobweb/user/repository/UserRepository.java
+++ backend/src/main/java/com/jy/jobweb/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.jy.jobweb.user.repository;
 
 import com.jy.jobweb.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 // repository
